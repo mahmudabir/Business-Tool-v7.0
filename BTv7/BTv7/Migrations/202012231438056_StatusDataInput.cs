c3576589404a3de83e6700f65d567068e8cf814c
Abir Mahmud <amabirmahmud@gmail.com>
2020-12-23 20:45:11 +0600

    Initial Data Input

---
+++ BTv7/BTv7/Migrations/202012231438056_StatusDataInput.cs
@@ -0,0 +1,25 @@
+﻿namespace BTv7.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class StatusDataInput : DbMigration
+    {
+        public override void Up()
+        {
+            // Status Table's Data Input
+            Sql("INSERT INTO UserDesignations (Designation) VALUES ('ADMIN'), ('MANAGER'), ('SALESMAN'), ('DELIVERYMAN'), ('CUSTOMER'), ('VENDOR')");
+            Sql("INSERT INTO AccessStatus (Status) VALUES ('ALLOWED'), ('RESTRICTED')");
+            Sql("INSERT INTO DeliveryStatus (Status) VALUES ('DELIVERED'), ('RETURNED')");
+            Sql("INSERT INTO MessageStatus (Status) VALUES ('SEEN'), ('UNSEEN')");
+            Sql("INSERT INTO OrderStatus (Status) VALUES ('UNAPPROVED'), ('APPROVED'), ('CANCELED'), ('ACCEPTED'), ('RETURNED')");
+            Sql("INSERT INTO SaleTypes (Type) VALUES ('ONLINE'), ('OFFLINE')");
+            Sql("INSERT INTO ProductStatus (Status) VALUES ('AVAILABLE'), ('UNAVAILABLE')");
+            Sql("INSERT INTO RegistrationStatus (Status) VALUES ('PENDING'), ('APPROVED'), ('CANCELED')");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ BTv7/BTv7/Migrations/202012231440293_LoginDataInput.cs
@@ -0,0 +1,28 @@
+﻿namespace BTv7.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class LoginDataInput : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Logins (Username, Email, Mobile, Password, AccessStatusID, UserDesignationID, RegistrationStatusID) " +
+                "VALUES ('1', 'admin@gmail.com', '+8801234567890', '1111', 1, 1, 2)");
+            Sql("INSERT INTO Logins (Username, Email, Mobile, Password, AccessStatusID, UserDesignationID, RegistrationStatusID) " +
+                "VALUES ('2', 'manager@gmail.com', '+8801234567890', '2222', 1, 1, 2)");
+            Sql("INSERT INTO Logins (Username, Email, Mobile, Password, AccessStatusID, UserDesignationID, RegistrationStatusID) " +
+                "VALUES ('3', 'salesman@gmail.com', '+8801234567890', '3333', 1, 1, 2)");
+            Sql("INSERT INTO Logins (Username, Email, Mobile, Password, AccessStatusID, UserDesignationID, RegistrationStatusID) " +
+                "VALUES ('4', 'deliveryman@gmail.com', '+8801234567890', '4444', 1, 1, 2)");
+            Sql("INSERT INTO Logins (Username, Email, Mobile, Password, AccessStatusID, UserDesignationID, RegistrationStatusID) " +
+                "VALUES ('5', 'customer@gmail.com', '+8801234567890', '5555', 1, 1, 2)");
+            Sql("INSERT INTO Logins (Username, Email, Mobile, Password, AccessStatusID, UserDesignationID, RegistrationStatusID) " +
+                "VALUES ('6', 'vendor@gmail.com', '+8801234567890', '6666', 1, 1, 2)");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
