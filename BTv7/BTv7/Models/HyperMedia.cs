3f3239bc72d57e000d80f7cb47ab708b4b18affe
Abir Mahmud <amabirmahmud@gmail.com>
2020-12-25 00:29:02 +0600

    All Repository Initialized

---
+++ BTv7/BTv7/Models/HyperMedia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTv7.Models
+{
+    public class HyperMedia
+    {
+        public string Rel { get; set; }
+
+        public string Href { get; set; }
+        public string Method { get; set; }
+    }
+}
---
+++ BTv7/BTv7/Models/LinkExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace BTv7.Models
+{
+    public static class LinkExtensions
+    {
+        public static dynamic AddLinks<T>(this T content, params object[] links)
+        {
+            IDictionary<string, object> result = new ExpandoObject();
+
+            typeof(T)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .ToList()
+                .ForEach(_ => result[_.Name.ToLower()] = _.GetValue(content));
+
+            result["links"] = links;
+
+            return result;
+        }
+    }
+}
