26c9cddf2accebd71dda04ed4bbf7be6265b0f2e
Abir Mahmud <amabirmahmud@gmail.com>
2020-12-24 20:08:30 +0600

    User Repositories Added

---
+++ BTv7/BTv7/Repositories/CustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using BTv7.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTv7.Repositories
+{
+    public class CustomerRepository : Repository<Customer>
+    {
+    }
+}
