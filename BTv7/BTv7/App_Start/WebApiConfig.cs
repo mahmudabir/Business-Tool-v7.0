b70be1f40b3b77ebb13cb43bb83bc02a7bf35dd5
Abir Mahmud <amabirmahmud@gmail.com>
2020-12-24 19:42:31 +0600

    Generic Repository, CORS, JSON Format Added

--- BTv7/BTv7/App_Start/WebApiConfig.cs
+++ BTv7/BTv7/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace BTv7
 {
@@ -19,6 +20,13 @@ namespace BTv7
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+            //config.Formatters.Remove(config.Formatters.JsonFormatter);
+
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
         }
     }
 }
