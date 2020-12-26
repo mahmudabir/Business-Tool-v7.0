2c39b381b35dbe35c6f24e3ffec28e535f26196a
imranalways <imran13854@gmail.com>
2020-12-26 23:26:05 +0600

    Chat Api added

---
+++ Back_End/BTv7/BTv7/Controllers/ChatsController.cs
@@ -0,0 +1,63 @@
+﻿using BTv7.Models;
+using BTv7.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BTv7.Controllers
+{
+    [RoutePrefix("api/chats")]
+    public class ChatsController : ApiController
+    {
+        [Route("{id}", Name = "GetUser")]
+        [BasicAuthentication]
+        public IHttpActionResult Get(string id)
+        {
+            LoginRepository loginrepo = new LoginRepository();
+            var user=loginrepo.GetUserByUsername(id);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return StatusCode(HttpStatusCode.NotFound);
+            }
+        }
+
+        [Route("{sid}/{rid}", Name = "GetChat")]
+        [BasicAuthentication]
+        public IHttpActionResult Get(int sid,int rid)
+        {
+            ChatRepository chatrepo = new ChatRepository();
+            var chatList = chatrepo.GetChatBySenderReceiver(sid, rid);
+            if (chatList != null)
+            {
+                MessageRepository msgrepo = new MessageRepository();
+                var msgList=msgrepo.GetMessageByChatId(chatList.ID);
+                if (msgList.Count()>0)
+                {
+                    return Ok(msgList);
+                }
+                else
+                {
+                    return StatusCode(HttpStatusCode.NoContent);
+                }
+               
+
+            }
+            else
+            {
+                Chat chat = new Chat();
+                chat.SenderID = sid;
+                chat.ReceiverID = rid;
+                chatrepo.Insert(chat);
+                return Ok(chat);
+            }
+        }
+
+    }
+}
--- Back_End/BTv7/BTv7/Repositories/ChatRepository.cs
+++ Back_End/BTv7/BTv7/Repositories/ChatRepository.cs
@@ -8,5 +8,11 @@ namespace BTv7.Repositories
 {
     public class ChatRepository : Repository<Chat>
     {
+
+        public Chat GetChatBySenderReceiver(int sid,int rid)
+        {
+            List<Chat> chatFromDB = this.GetAll();
+            return chatFromDB.FirstOrDefault(x => x.SenderID.Equals(sid) && x.ReceiverID.Equals(rid) || x.SenderID.Equals(rid) && x.ReceiverID.Equals(sid));
+        }
     }
 }
