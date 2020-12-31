8990dec40e6a397ec52df72c1220b344e024b4f1
hasib999 <45915540+hasib999@users.noreply.github.com>
2020-12-31 20:39:55 +0600

    Login and Signup null Validation
    using Jquery

--- Front_End/JS/Login/Authentication.js
+++ Front_End/JS/Login/Authentication.js
@@ -24,9 +24,6 @@ $(document).ready(function () {
         $("#divlogin").show();
     })
 
-
-
-
     var loadLogin = function () {
         $.ajax({
             url: "https://localhost:44308/api/logins/login",
@@ -39,59 +36,72 @@ $(document).ready(function () {
                 'Authorization': 'Basic ' + btoa($("#username").val() + ":" + $("#password").val()),
             },
             complete: function (xhr, status) {
-                if (xhr.status == 200) {
-
-                    localStorage.authUser = btoa($("#username").val() + ":" + $("#password").val());
-
-                    console.log(xhr);
-
-
-                    var user = xhr.responseJSON;
-                    localStorage.userId = user.id;
-                    localStorage.username = user.username;
-                    localStorage.userRole = user.userDesignationID;
-
-
-                    console.log(localStorage.userId);
-                    console.log(localStorage.username);
-                    console.log(localStorage.authUser);
-                    console.log("Login Success");
-
-                    console.log(xhr);
-
-                    //USER TYPE WISE REDIRECTION
-                    if (localStorage.userRole == 1) {
-                        window.location.href = "../../html/Admin";
-                        $("#msg").html("<div class=\"alert alert-success\" role=\"alert\">Success: Successfully Logged In</div>");
-                    }
-                    else if (localStorage.userRole == 2) {
-                        window.location.href = "../../html/Manager";
-                        $("#msg").html("<div class=\"alert alert-success\" role=\"alert\">Success: Successfully Logged In</div>");
-                    }
-                    else if (localStorage.userRole == 3) {
-                        window.location.href = "../../html/Salesman";
-                        $("#msg").html("<div class=\"alert alert-success\" role=\"alert\">Success: Successfully Logged In</div>");
-                    }
-                    else if (localStorage.userRole == 4) {
-                        window.location.href = "../../html/Deliveryman";
-                        $("#msg").html("<div class=\"alert alert-success\" role=\"alert\">Success: Successfully Logged In</div>");
-                    }
-                    else if (localStorage.userRole == 5) {
-                        window.location.href = "../../html/Customer";
-                        $("#msg").html("<div class=\"alert alert-success\" role=\"alert\">Success: Successfully Logged In</div>");
-                    }
-                    else if (localStorage.userRole == 6) {
-                        window.location.href = "../../html/Vendor";
-                        $("#msg").html("<div class=\"alert alert-success\" role=\"alert\">Success: Successfully Logged In</div>");
+                if($("#username").val()!="" && $("#password").val()!="")
+                {
+                    if (xhr.status == 200) {
+                        localStorage.authUser = btoa($("#username").val() + ":" + $("#password").val());
+    
+                        console.log(xhr);
+    
+                        var user = xhr.responseJSON;
+                        localStorage.userId = user.id;
+                        localStorage.username = user.username;
+                        localStorage.userRole = user.userDesignationID;
+    
+    
+                        console.log(localStorage.userId);
+                        console.log(localStorage.username);
+                        console.log(localStorage.authUser);
+                        console.log("Login Success");
+    
+                        console.log(xhr);
+    
+                        //USER TYPE WISE REDIRECTION
+                        if (localStorage.userRole == 1) {
+                            window.location.href = "../../html/Admin";
+                            $("#msg").html("<div class=\"alert alert-success\" role=\"alert\">Success: Successfully Logged In</div>");
+                        }
+                        else if (localStorage.userRole == 2) {
+                            window.location.href = "../../html/Manager";
+                            $("#msg").html("<div class=\"alert alert-success\" role=\"alert\">Success: Successfully Logged In</div>");
+                        }
+                        else if (localStorage.userRole == 3) {
+                            window.location.href = "../../html/Salesman";
+                            $("#msg").html("<div class=\"alert alert-success\" role=\"alert\">Success: Successfully Logged In</div>");
+                        }
+                        else if (localStorage.userRole == 4) {
+                            window.location.href = "../../html/Deliveryman";
+                            $("#msg").html("<div class=\"alert alert-success\" role=\"alert\">Success: Successfully Logged In</div>");
+                        }
+                        else if (localStorage.userRole == 5) {
+                            window.location.href = "../../html/Customer";
+                            $("#msg").html("<div class=\"alert alert-success\" role=\"alert\">Success: Successfully Logged In</div>");
+                        }
+                        else if (localStorage.userRole == 6) {
+                            window.location.href = "../../html/Vendor";
+                            $("#msg").html("<div class=\"alert alert-success\" role=\"alert\">Success: Successfully Logged In</div>");
+                        }
+                        else {
+                            $("#msg").html("<div class=\"alert alert-danger\" role=\"alert\">Error : " + xhr.responseJSON.message + "</div>");
+                        }
                     }
                     else {
+                        console.log(xhr);
                         $("#msg").html("<div class=\"alert alert-danger\" role=\"alert\">Error : " + xhr.responseJSON.message + "</div>");
                     }
                 }
-                else {
-                    console.log(xhr);
-                    $("#msg").html("<div class=\"alert alert-danger\" role=\"alert\">Error : " + xhr.responseJSON.message + "</div>");
+                else
+                {
+                    if($("#username").val()=="")
+                    {
+                        $("#msg1").html("*Username Can't be Empty");
+                    }                    
+                    if($("#password").val()=="")
+                    {
+                        $("#msg2").html("*Password Can't be Empty");
+                    }
                 }
+
             }
         });
     }
@@ -140,12 +150,36 @@ $(document).ready(function () {
                 'Authorization': 'Basic ' + btoa($("#regusername").val() + ":" + $("#regpassword").val()),
             },
             complete: function (xhr, status) {
-                if (xhr.status == 201) {
-                    console.log("Login Table Data insert Success");
-                    insertInCustomer();
+                if($("#regname").val()!="" && $("#regemail").val()!="" && $("#regmobile").val()!="" && $("#regusername").val()!="" && $("#regpassword").val()!="")
+                {
+                    if (xhr.status == 201) {
+                        console.log("Login Table Data insert Success");
+                        insertInCustomer();
+                    }
+                    else {
+                        console.log(xhr);
+                    }
                 }
-                else {
-                    console.log(xhr);
+                else
+                {
+                    if($("#regname").val()=="")
+                    {
+                        $("#msg11").html("*Full Name Can't be Empty");
+                    }                    
+                    if($("#regemail").val()=="")
+                    {
+                        $("#msg21").html("*Email Can't be Empty");
+                    }
+                    if($("#regmobile").val()=="")
+                    {
+                        $("#msg3").html("*Mobile No. Can't be Empty");
+                    }if($("#regusername").val()=="")
+                    {
+                        $("#msg4").html("*Username Can't be Empty");
+                    }if($("#regpassword").val()=="")
+                    {
+                        $("#msg5").html("*password Can't be Empty");
+                    }
                 }
             }
         });
@@ -158,7 +192,7 @@ $(document).ready(function () {
 
 
     $("#btnlogin").click(function () {
-        loadLogin();
+        loadLogin();  
     });
     $("#btnregister").click(function () {
         loadRegister();
