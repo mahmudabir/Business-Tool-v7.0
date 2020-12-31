c8844aed3939ed631c6e54772b2a640a5498e7de
imranalways <imran13854@gmail.com>
2020-12-31 20:35:03 +0600

    Deliveryman & Salesman Index Added

---
+++ Front_End/JS/Salesman/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	$("#div7").animate({right:"10px"},800,function(){
+	$("#div6").animate({right:"40px"},800,function(){
+	$("#div5").animate({right:"80px"},800,function(){
+	$("#div4").animate({right:"110px"},800,function(){
+	$("#div3").animate({right:"140px"},800,function(){
+	$("#div2").animate({right:"170px"},800,function(){
+	$("#div1").animate({right:"200px"},800,function(){
+	$("#div8").animate({right:"-15px"},800,function(){
+	
+	});
+	});
+	});
+	});
+	});
+	});
+	});
+	});
+
+});
