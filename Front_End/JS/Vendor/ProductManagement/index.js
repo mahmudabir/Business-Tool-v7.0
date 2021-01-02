e16d3a5d8564532a3d0a52c86d95a77b6cd97251
hasib999 <45915540+hasib999@users.noreply.github.com>
2021-01-02 21:46:23 +0600

    Product search complete

--- Front_End/JS/Vendor/ProductManagement/index.js
+++ Front_End/JS/Vendor/ProductManagement/index.js
@@ -22,19 +22,11 @@ $(document).ready(function(){
                     var data = xhr.responseJSON;
 
                     var str = '';
-                    var icon = "";
                     if(data.length>0)
                     {
                         for (var i = 0; i < data.length; i++) 
                         {
-                            if(data[i].login.accessStatusID == 1)
-                            {
-                                icon = '<i style="color: green;" class="fas fa-user-check"></i>';
-                            } 
-                            else
-                            {
-                                icon = '<i style="color: red;" class="fas fa-user-slash"></i>';
-                            }
+                            
                             str += "<tr>"+
                                         "<td align='center'>"+ data[i].id + "</td>"+
                                         "<td>"+ data[i].name +"</td>"+
@@ -42,8 +34,9 @@ $(document).ready(function(){
                                         "<td>" + data[i].buyPrice + "</td>"+
                                         "<td>" + data[i].sellPrice + "</td>"+
                                         "<td>" + data[i].productType.type + "</td>"+
+                                        "<td>" + data[i].productStatus.status + "</td>"+
                                         
-                                        "<td align='center'> <button type='button' data-toggle='modal' data-target='#detailProduct' data-id="+data[i].id+" class='btn btn-outline-dark'>Details</button>" +
+                                        "<td align='center'> <button type='button' data-toggle='modal' data-target='#detailProduct' data-id="+data[i].id+" class='btn btn-dark'>Details</button>" +
                                 "</tr>";
                         }
                     }
@@ -62,5 +55,69 @@ $(document).ready(function(){
         });
     }
     loadAllProducts();
+
+
+    // search by name
+    var productSearch = function () {
+        if($.trim($("#search").val()) != "")
+        {
+            $.ajax({
+                url: "https://localhost:44308/api/products/name/"+$("#search").val(),
+                method: "GET",
+                headers: {
+                    'Authorization': 'Basic ' + localStorage.authUser,
+                },
+                complete: function (xhr, status) {
+                    if (xhr.status == 200) {
+                        console.log(xhr.responseJSON);
+    
+                        var data = xhr.responseJSON;
+    
+                        var str = '';
+                        if(data.length>0)
+                        {
+                            for (var i = 0; i < data.length; i++) 
+                            {
+                                
+                                
+                                str += "<tr>"+
+                                    "<td align='center'>"+ data[i].id + "</td>"+
+                                    "<td>"+ data[i].name +"</td>"+
+                                    "<td>"+ data[i].quantity+ "</td>"+
+                                    "<td>" + data[i].buyPrice + "</td>"+
+                                    "<td>" + data[i].sellPrice + "</td>"+
+                                    "<td>" + data[i].productType.type + "</td>"+
+                                    "<td>" + data[i].productStatus.status + "</td>"+
+                                    
+                                    "<td align='center'> <button type='button' data-toggle='modal' data-target='#detailProduct' data-id="+data[i].id+" class='btn btn-dark'>Details</button>" +
+                                "</tr>";
+                                
+                            }
+                        }
+                        else
+                        {
+                            str += "<tr><td colspan='6' align='middle'>NO DATA FOUND</td></tr>";
+                        }
+
+                        $("#table tbody").html(str);
+                    }
+                    else {
+                        alert("Something Went Wrong.");
+                    }
+                }
+            });
+        }
+        
+        else
+        {
+            loadAllProducts();
+        }
+    }
+    $("#search").on("keyup change",function(){
+        productSearch();
+    });
+
+
+    //search by name
 
 })
