0f9c7e0a3ef27a1da958a774ac76de6ebbd74c3c
hasib999 <45915540+hasib999@users.noreply.github.com>
2021-01-05 04:50:41 +0600

    bug fixed

--- Front_End/JS/Vendor/index.js
+++ Front_End/JS/Vendor/index.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-       
+    if (localStorage.authUser == null || localStorage.userRole!=6)
+    {
+        //localStorage.clear();
+        window.location.href = "../Login/index.html";
+    }
     $('#content').load("vendornav.html");
 
 
