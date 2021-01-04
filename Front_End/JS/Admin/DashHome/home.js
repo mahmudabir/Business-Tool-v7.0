54e7e92c6eaecdf8870fac57b07d9c31d92115a0
Tanvir Tanjum Shourav <39719570+tanvirtanjum@users.noreply.github.com>
2021-01-04 20:12:26 +0600

    Update home.js

--- Front_End/JS/Admin/DashHome/home.js
+++ Front_End/JS/Admin/DashHome/home.js
@@ -40,6 +40,9 @@ $(document).ready(function(){
                                 'rgba(255, 206, 86, 0.2)',
                                 'rgba(75, 192, 192, 0.2)',
                                 'rgba(153, 102, 255, 0.2)',
+                                'rgba(53, 102, 255, 0.2)',
+                                'rgba(73, 20, 255, 0.2)',
+                                'rgba(3, 102, 255, 0.2)',
                                 'rgba(200, 159, 64, 0.2)'
                             ],
                             borderColor: [
@@ -68,7 +71,7 @@ $(document).ready(function(){
 
                     // Chart declaration:
                     var myBarChart = new Chart(ctx, {
-                        type: 'pie',
+                        type: 'bar',
                         data: data,
                         options: options
                     });
@@ -109,17 +112,27 @@ $(document).ready(function(){
 
                     var ctx = document.getElementById('myChart2').getContext('2d');
                     var myChart = new Chart(ctx, {
-                        type: 'line',
+                        type: 'bar',
                         data: {
                             labels: data[0], //['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
                             datasets: [{
-                                label: 'Amount(BDT)',
+                                label: data[1],
                                 data: data[1], //[12, 19, 3, 5, 2, 3],
                                 backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)',
                                     'rgba(54, 162, 235, 0.2)',
+                                    'rgba(255, 206, 86, 0.2)',
+                                    'rgba(75, 192, 192, 0.2)',
+                                    'rgba(153, 102, 255, 0.2)',
+                                    'rgba(200, 159, 64, 0.2)'
                                 ],
                                 borderColor: [
                                     'rgba(255, 99, 132, 1)',
+                                    'rgba(54, 162, 235, 1)',
+                                    'rgba(255, 206, 86, 1)',
+                                    'rgba(75, 192, 192, 1)',
+                                    'rgba(153, 102, 255, 1)',
+                                    'rgba(200, 159, 64, 1)'
                                 ],
                                 borderWidth: 1
                             }]
