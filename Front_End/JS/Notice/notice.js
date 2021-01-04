f2d2521e44e495f70aee510fc372a0d3ab7f459f
Abir Mahmud <amabirmahmud@gmail.com>
2021-01-04 21:24:19 +0600

    salesman navbar fixed

--- Front_End/JS/Notice/notice.js
+++ Front_End/JS/Notice/notice.js
@@ -1,34 +1,27 @@
-$(document).ready(function(){
-    if (localStorage.authUser == null || localStorage.userRole==5)
-    {
+$(document).ready(function () {
+    if (localStorage.authUser == null || localStorage.userRole == 5) {
         //localStorage.clear();
         window.location.href = "http://localhost/Business-Tool-v7.0-api/Front_End/Html/Login/";
     }
 
-	if(localStorage.userRole==1)
-	{
-		$('#content').load("../Admin/adminnav.html");
-	}
-	else if(localStorage.userRole==2)
-	{
-		$('#content').load("../Manager/managernav.html");
-	}
-	else if(localStorage.userRole==3)
-	{
-		$('#content').load("../Salesman/SalesmanNavbar.html");
-	}
-	else if(localStorage.userRole==4)
-	{
-		$('#content').load("../Deliveryman/deliverymannav.html");
-	}
-	else if(localStorage.userRole==6)
-	{
-		$('#content').load("../Vendor/vendornav.html");
-	}
-	else
-	{
-		window.location.href = "http://localhost/Business-Tool-v7.0-api/Front_End/Html/Login/";
-	}
+    if (localStorage.userRole == 1) {
+        $('#content').load("../Admin/adminnav.html");
+    }
+    else if (localStorage.userRole == 2) {
+        $('#content').load("../Manager/managernav.html");
+    }
+    else if (localStorage.userRole == 3) {
+        $('#content').load("../Salesman/salesmannav.html");
+    }
+    else if (localStorage.userRole == 4) {
+        $('#content').load("../Deliveryman/deliverymannav.html");
+    }
+    else if (localStorage.userRole == 6) {
+        $('#content').load("../Vendor/vendornav.html");
+    }
+    else {
+        window.location.href = "http://localhost/Business-Tool-v7.0-api/Front_End/Html/Login/";
+    }
 
     //LOAD LIST
     var loadAllNotice = function () {
@@ -46,91 +39,85 @@ $(document).ready(function(){
 
                     var str = '';
                     var sl = 1;
-                    if(data.length>0)
-                    {
-                        for (var i = 0; i < data.length; i++) 
-                        {
-							sl=i+1;
-                            if(sl = 1)
-							{
-								str += "<div class='card'>"+
-										"<div class='card-header' id='heading"+data[i].id+"One"+"'>"+
-											"<h2 class='mb-0'>"+
-												"<button class='btn btn-link' data-toggle='collapse' data-target='#collapse"+data[i].id+"One"+"' aria-expanded='false' aria-controls='collapse"+data[i].id+"'>"+
-													data[i].subject+" &nbsp;&nbsp;&nbsp; <span style='font-style: italic; color:#b3b3b3'>(<b>@"+data[i].postDate+"</b>)</span>"+
-												"</button>"+
-											"</h2>"+
-										"</div>"+
-									
-										"<div id='collapse"+data[i].id+"One"+"' class='collapse show' aria-labelledby='heading"+data[i].id+"One"+"' data-parent='#accordion'>"+
-											"<div class='card-body'>"+
-												data[i].description	+
-											"</div>"+
-										"</div>"+
-									"</div><br>";
-									//sl++;
-							}
-							else
-							{
-								str += "<div class='card'>"+
-										"<div class='card-header' id='heading"+data[i].id+"One"+"'>"+
-											"<h5 class='mb-0'>"+
-												"<button class='btn btn-link' data-toggle='collapse' data-target='#collapse"+data[i].id+"One"+"' aria-expanded='false' aria-controls='collapse"+data[i].id+"'>"+
-													data[i].subject+" &nbsp;&nbsp;&nbsp; <span style='font-style: italic; color:#b3b3b3'>(<b>@"+data[i].postDate+"</b>)</span>"+
-												"</button>"+
-											"</h5>"+
-										"</div>"+
-									
-										"<div id='collapse"+data[i].id+"One"+"' class='collapse show' aria-labelledby='heading"+data[i].id+"One"+"' data-parent='#accordion'>"+
-											"<div class='card-body'>"+
-												data[i].description	+
-											"</div>"+
-										"</div>"+
-									"</div><br>";
-									//sl++;
-							}
-							sl=i+1;
+                    if (data.length > 0) {
+                        for (var i = 0; i < data.length; i++) {
+                            sl = i + 1;
+                            if (sl = 1) {
+                                str += "<div class='card'>" +
+                                    "<div class='card-header' id='heading" + data[i].id + "One" + "'>" +
+                                    "<h2 class='mb-0'>" +
+                                    "<button class='btn btn-link' data-toggle='collapse' data-target='#collapse" + data[i].id + "One" + "' aria-expanded='false' aria-controls='collapse" + data[i].id + "'>" +
+                                    data[i].subject + " &nbsp;&nbsp;&nbsp; <span style='font-style: italic; color:#b3b3b3'>(<b>@" + data[i].postDate + "</b>)</span>" +
+                                    "</button>" +
+                                    "</h2>" +
+                                    "</div>" +
+
+                                    "<div id='collapse" + data[i].id + "One" + "' class='collapse show' aria-labelledby='heading" + data[i].id + "One" + "' data-parent='#accordion'>" +
+                                    "<div class='card-body'>" +
+                                    data[i].description +
+                                    "</div>" +
+                                    "</div>" +
+                                    "</div><br>";
+                                //sl++;
+                            }
+                            else {
+                                str += "<div class='card'>" +
+                                    "<div class='card-header' id='heading" + data[i].id + "One" + "'>" +
+                                    "<h5 class='mb-0'>" +
+                                    "<button class='btn btn-link' data-toggle='collapse' data-target='#collapse" + data[i].id + "One" + "' aria-expanded='false' aria-controls='collapse" + data[i].id + "'>" +
+                                    data[i].subject + " &nbsp;&nbsp;&nbsp; <span style='font-style: italic; color:#b3b3b3'>(<b>@" + data[i].postDate + "</b>)</span>" +
+                                    "</button>" +
+                                    "</h5>" +
+                                    "</div>" +
+
+                                    "<div id='collapse" + data[i].id + "One" + "' class='collapse show' aria-labelledby='heading" + data[i].id + "One" + "' data-parent='#accordion'>" +
+                                    "<div class='card-body'>" +
+                                    data[i].description +
+                                    "</div>" +
+                                    "</div>" +
+                                    "</div><br>";
+                                //sl++;
+                            }
+                            sl = i + 1;
                         }
                     }
-                    else
-                    {
-                        str += "<div class='card'>"+
-										"<div class='card-header' id='headingOne'>"+
-											"<h5 class='mb-0'>"+
-												"<button class='btn btn-link' data-toggle='collapse' data-target='#collapseOne' aria-expanded='false' aria-controls='collapse"+data[i].id+"'>"+
-													"NO NOTICE"+
-												"</button>"+
-											"</h5>"+
-										"</div>"+
-									
-										"<div id='collapseOne' class='collapse show' aria-labelledby='headingOne' data-parent='#accordion'>"+
-											"<div class='card-body'>"+
-												"NO NOTICE"+
-											"</div>"+
-										"</div>"+
-									"</div><br>";
+                    else {
+                        str += "<div class='card'>" +
+                            "<div class='card-header' id='headingOne'>" +
+                            "<h5 class='mb-0'>" +
+                            "<button class='btn btn-link' data-toggle='collapse' data-target='#collapseOne' aria-expanded='false' aria-controls='collapse" + data[i].id + "'>" +
+                            "NO NOTICE" +
+                            "</button>" +
+                            "</h5>" +
+                            "</div>" +
+
+                            "<div id='collapseOne' class='collapse show' aria-labelledby='headingOne' data-parent='#accordion'>" +
+                            "<div class='card-body'>" +
+                            "NO NOTICE" +
+                            "</div>" +
+                            "</div>" +
+                            "</div><br>";
                     }
-					// alert(sl);
+                    // alert(sl);
                     $("#accordion").append(str);
                 }
-                else 
-                {
-                    str += "<div class='card'>"+
-								"<div class='card-header' id='headingOne'>"+
-									"<h5 class='mb-0'>"+
-										"<button class='btn btn-link' data-toggle='collapse' data-target='#collapseOne' aria-expanded='false' aria-controls='collapse"+data[i].id+"'>"+
-											"NO NOTICE"+
-										"</button>"+
-									"</h5>"+
-								"</div>"+
-							
-								"<div id='collapseOne' class='collapse show' aria-labelledby='headingOne' data-parent='#accordion'>"+
-									"<div class='card-body'>"+
-										"NO NOTICE"+
-									"</div>"+
-								"</div>"+
-							"</div><br>";
-					$("#accordion").append(str);
+                else {
+                    str += "<div class='card'>" +
+                        "<div class='card-header' id='headingOne'>" +
+                        "<h5 class='mb-0'>" +
+                        "<button class='btn btn-link' data-toggle='collapse' data-target='#collapseOne' aria-expanded='false' aria-controls='collapse" + data[i].id + "'>" +
+                        "NO NOTICE" +
+                        "</button>" +
+                        "</h5>" +
+                        "</div>" +
+
+                        "<div id='collapseOne' class='collapse show' aria-labelledby='headingOne' data-parent='#accordion'>" +
+                        "<div class='card-body'>" +
+                        "NO NOTICE" +
+                        "</div>" +
+                        "</div>" +
+                        "</div><br>";
+                    $("#accordion").append(str);
                 }
             }
         });
